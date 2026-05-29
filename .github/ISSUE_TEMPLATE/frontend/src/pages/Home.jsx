44909a304aa0a39f3e2e84b5b764dc7762868dbb
olsjs7 <lovegraves77@gmail.com>
2026-05-29 22:01:35 +0900

    프론트엔드 초기 세팅 및 로그인 회원가입 구현

---
+++ .github/ISSUE_TEMPLATE/frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-10">
+      <h1 className="text-4xl font-bold">
+        홈 화면
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
