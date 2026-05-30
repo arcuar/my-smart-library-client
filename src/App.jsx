dd8c7bd971a08307a2b17356eaa88b5d23564a60
arcuar <nunnaerim@gmail.com>
2026-05-30 14:47:55 +0900

    mylibrary 오타 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Search from "./pages/Search";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import MyLibrary from "./pages/MyLibrary";
+import MyLibrary from "./pages/Mylibrary";
 
 function App() {
   return (
